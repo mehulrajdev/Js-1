07b0b47ec4a9e9502cf85e4049aca31d58a1f477
mehulrajdev <mehulrajpnbe@gmail.com>
2022-12-17 11:01:09 +0530

    added sol.js

---
+++ sol.js
@@ -0,0 +1,9 @@
+let courseName = "web development";
+let coursePrice = 450;
+let courseGoals = ["learn web development","become developer","have fun"];
+
+let onlineCourse ={
+    name:courseName,
+    price:coursePrice,
+    goals:courseGoals
+};
