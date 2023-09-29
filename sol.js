55870f7cdc566503047f036f4dfaa6b492148bdc
Mehul <82897404+mehulrajdev@users.noreply.github.com>
2023-09-29 11:47:55 +0530

    Update sol.js

--- sol.js
+++ sol.js
@@ -1,6 +1,6 @@
 let courseName = "web development";
 let coursePrice = 450;
-let courseGoals = ["learn web development","become developer","have fun"];
+let courseGoals = ["learn web development","become developer","enjoy"];
 
 let onlineCourse ={
     name:courseName,
